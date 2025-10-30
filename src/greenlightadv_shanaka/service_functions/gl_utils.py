e6c9a4dd1d1748c3c9c34800dda12634fe12c0eb
Shanaka Prageeth <Shanakaprageeth@gmail.com>
2025-10-30 12:58:35 +0000

    wip: improve gl data aggregation and logging

--- src/greenlightadv_shanaka/service_functions/gl_utils.py
+++ src/greenlightadv_shanaka/service_functions/gl_utils.py
@@ -24,25 +24,44 @@ def aggregate_gl_data(gl_previous, gl_next):
         return gl_next
 
     gl_aggregated = gl_previous.copy()
-
-    for key, value in gl_next['x'].items():
-        if key is 't':
-            gl_aggregated['x'][key] = np.array([gl_previous['x']['t'][0], value[1]])
-        if gl_previous['x'].get(key) is None:
-            gl_aggregated['x'][key] = value
-            logger.info(f"Key '{key}' not in previous data. Added current data directly.")
-            continue
-        if isinstance(value, dict):
-            gl_aggregated['x'][key] = np.array([gl_previous['x'][key][0], value[1]])
-        else:
-            last_time = np.max(gl_previous['x'][key], axis=0)[0]
-            logging.info(f"gl_previous['x'][{key}][0][0]: {gl_previous['x'][key][0][0]}, gl_previous['x'][{key}][0][1]: {gl_previous['x'][key][0][1]}")
-            step_size = gl_previous['x'][key][0][0] - gl_previous['x'][key][1][0] if len(gl_previous['x'][key]) > 1 else 1.0
-            # add offset to align time
-            offset_value = last_time + step_size
-            logging.info(f"Aggregating key '{key}': last_time={last_time}, step_size={step_size}, offset_value={offset_value}")
-            value[:, 0] += offset_value
-            gl_aggregated['x'][key]= np.concatenate((gl_previous['x'][key], value), axis=0)
+    # todo handle 'u ''p' 't'
+    for main_key, main_value in gl_next.items():
+        if main_key is 't':
+            gl_aggregated[main_key] = np.array([gl_previous['t'][0], main_value[1]])
+        if main_key is 'p':
+            gl_aggregated[main_key] = main_value
+        for sub_key, sub_value in gl_next[main_key].items():
+            if gl_previous[main_key].get(sub_key) is None:
+                gl_aggregated[main_key][sub_key] = sub_value
+                logger.info(f"Key '{sub_key}' not in previous data. Added current data directly.")
+                continue
+            else:
+                logger.info(f"Aggregating data for key '{sub_key} {type(sub_value)}'")
+                if isinstance(sub_value, dict):
+                    logger.info(f"Aggregating nested dictionary for key '{sub_key}'")
+                    for internal_key, internal_value in gl_next[main_key][sub_key].items():
+                        if gl_previous[main_key][sub_key][internal_key].get(sub_key) is None:
+                            gl_aggregated[main_key][sub_key][internal_key] = internal_value
+                            logger.info(f"Key '{internal_key}' not in previous data. Added current data directly.")
+                            continue
+                        else:
+                            last_time = np.max(gl_previous[main_key][sub_key][internal_key], axis=0)[0]
+                            #logging.info(f"gl_previous[main_key][{sub_key}][{internal_key}][0][0]: {gl_previous[main_key][{sub_key}][{internal_key}][0][0]}, gl_previous[main_key][{sub_key}][{internal_key}][0][1]: {gl_previous[main_key][{sub_key}][{internal_key}][0][1]}")
+                            step_size = gl_previous[main_key][sub_key][internal_key][0][0] - gl_previous[main_key][sub_key][internal_key][1][0] if len(gl_previous[main_key][sub_key][internal_key]) > 1 else 1.0
+                            # add offset to align time
+                            offset_value = last_time + step_size
+                            #logging.info(f"Aggregating key '{sub_key}': last_time={last_time}, step_size={step_size}, offset_value={offset_value}")
+                            sub_value[:, 0] += offset_value
+                            gl_aggregated[main_key][sub_key][internal_key]= np.concatenate((gl_previous[main_key][sub_key][internal_key], internal_value), axis=0)
+                else:
+                    last_time = np.max(gl_previous[main_key][sub_key], axis=0)[0]
+                    #logging.info(f"gl_previous[main_key][{sub_key}][0][0]: {gl_previous[main_key][{sub_key}][0][0]}, gl_previous[main_key][{sub_key}][0][1]: {gl_previous[main_key][{sub_key}][0][1]}")
+                    step_size = gl_previous[main_key][sub_key][0][0] - gl_previous[main_key][sub_key][1][0] if len(gl_previous[main_key][sub_key]) > 1 else 1.0
+                    # add offset to align time
+                    offset_value = last_time + step_size
+                    #logging.info(f"Aggregating key '{sub_key}': last_time={last_time}, step_size={step_size}, offset_value={offset_value}")
+                    sub_value[:, 0] += offset_value
+                    gl_aggregated[main_key][sub_key]= np.concatenate((gl_previous[main_key][sub_key], sub_value), axis=0)
 
     logger.info("Aggregation of gl data successful.")
     return gl_aggregated
